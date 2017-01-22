3adeb73e78a5685b425b685d32e49450d5a3ff1e
Charles Wolfe <cwolfe@opinionlab.com>
2017-01-21 22:57:25 -0600

    Add fan.

--- src/goodberrys_flavor_of_the_day/util.clj
+++ src/goodberrys_flavor_of_the_day/util.clj
@@ -4,12 +4,12 @@
 (defn help-request []
   {:title "Help"
    :output (str "I know the flavor of the day at good berry's for every date "
-                "in the month.  Just say, alexa, ask good berry's what is "
+                "in the month.  Just say, alexa, ask good berry's fan what is "
                 "the flavor of the day. Do you want to ask me for a "
                 "date this month?")
    :reprompt-text (str "I know the flavor of the day at good berry's for "
                        "every date in the month.  Just say, alexa, ask "
-                       "good berry's what is the flavor of the day. "
+                       "good berry's fan what is the flavor of the day. "
                        "Do you want to ask me for a date this month?")
    :should-end-session false})
 
@@ -18,9 +18,9 @@
   {:title "Welcome"
    :output (str "Welcome to Goodberry's Flavor of the Day. "
                 "Please ask me for today's flavor of the day by saying, "
-                "Alexa, ask good berry's what is the flavor of the day "
+                "Alexa, ask good berry's fan what is the flavor of the day "
                 "today or on Friday.")
    :reprompt-text (str "Please ask me for today's flavor of the day by "
-                       "saying, Alexa, ask good berry's what is the flavor "
+                       "saying, Alexa, ask good berry's fan what is the flavor "
                        "of the day today or on Friday.")
    :should-end-session false})
